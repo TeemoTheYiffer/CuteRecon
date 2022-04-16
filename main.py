3f8a89cc3f5e8881045e7f0863a9a654ea716335
jaguirre3 <joe_aguirre@intuit.com>
2022-04-15 18:53:33 -0700

    Removed

--- main.py
+++
@@ -1,7 +0,0 @@
-# This is a fake py script to trigger GitGuardian alert
-
-encoded_api_key="b0F6OUI0THkyczdZY2lrSFcaZlgyYnzuOFzWS2zOMDUzM1RDbUcxag==" # Encoded
-
-import base64
-
-base_url="https://www.google.com"
