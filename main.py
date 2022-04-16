7f5686a5d724d104a5aa3cbb7c8df9edf2bee7d0
jaguirre3 <joe_aguirre@intuit.com>
2022-04-15 17:55:38 -0700

    Removed naked key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # This is a fake py script to trigger GitGuardian alert
 
-api_key="64efcb97-eda1-411b-928e-35e518dd0f3e" # Not Encoded
 encoded_api_key="b0F6OUI0THkyczdZY2lrSFcaZlgyYnzuOFzWS2zOMDUzM1RDbUcxag==" # Encoded
 
 import base64
